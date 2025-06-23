583aa0240efe4b78a9076343f7549f9f38f46d3f
Syed-56 <ssa.56.sultan@gmail.com>
2025-06-24 04:02:51 +0500

    Added Image Changing Logic

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const images = ['rock.png', 'paper.png', 'scissors.png'];
-function changeImage(finalImage) {
-    let imgElement = document.getElementById('mainImage');
+function changeImageHuman(finalImage) {
+    let imgElement = document.getElementById('mainImageHuman');
     let counter = 0;
-    let spinTime = 1000; // 1 second
-    let intervalTime = 100; // how fast it cycles
+    let spinTime = 2000; // 1 second
+    let intervalTime = 70; // how fast it cycles
+    let interval;
+
+    interval = setInterval(() => {
+        let randomIndex = Math.floor(Math.random() * images.length);
+        imgElement.src = images[randomIndex];
+        counter += intervalTime;
+
+        if (counter >= spinTime) {
+            clearInterval(interval);
+            imgElement.src = finalImage; // Set the final image after spinning
+        }
+    }, intervalTime);
+}
+
+function changeImageComputer() {
+    let finalImage = images[Math.floor(Math.random() * images.length)];
+    let imgElement = document.getElementById('mainImageComputer');
+    let counter = 0;
+    let spinTime = 2000; // 1 second
+    let intervalTime = 70; // how fast it cycles
     let interval;
 
     interval = setInterval(() => {
